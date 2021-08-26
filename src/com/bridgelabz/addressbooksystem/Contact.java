4e3f942d73d57d52c0d63bbb4eb457a9a891cbe9
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-26 16:22:14 +0530

    ADDED: Contact class for Address Book

---
+++ src/com/bridgelabz/addressbooksystem/Contact.java
@@ -0,0 +1,57 @@
+package com.bridgelabz.addressbooksystem;
+
+public class Contact {
+	
+	
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private int zipCode;
+	private int phoneNumber;
+	private String emailId;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+}
