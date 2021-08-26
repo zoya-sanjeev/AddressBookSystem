cd2b38e032480a6cfd647d63414868bdb35172d0
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-08-26 15:42:45 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.addressbooksystem;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book");
+
+	}
+
+}
