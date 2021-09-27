3730ebb720b79a9ae35a656d9cadf85a52927c64
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-27 18:47:44 +0530

    ADDED: AddressBookDBService created

--- src/main/com/bridgelabz/addressbooksystem/AddressBookDBService.java
+++ src/main/com/bridgelabz/addressbooksystem/AddressBookDBService.java
@@ -1,5 +1,20 @@
 package main.com.bridgelabz.addressbooksystem;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AddressBookDBService {
+	
+	private Connection getConnection() throws SQLException {
+		String jdbcURL="jdbc:mysql://localhost:3306/addressBookService?useSSL=false";
+		String userName="root";
+		String password="abcd1234";
+		Connection connection;
+		System.out.println("Connecting to database"+ jdbcURL);
+		connection=DriverManager.getConnection(jdbcURL,userName,password);
+		System.out.println("Connection is successfull"+ connection);
+		return connection;
+	}
 
 }
