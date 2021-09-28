6417f6f3e7fb09ecb2f76c748cc3afe65e41a0ad
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-28 23:11:59 +0530

    ADDED: created class Address;

---
+++ src/main/com/bridgelabz/addressbooksystem/Address.java
@@ -0,0 +1,35 @@
+package main.com.bridgelabz.addressbooksystem;
+
+public class Address {
+	
+	public String address;
+	public String city;
+	public String state;
+	public long zipCode;
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public long getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(long zipCode) {
+		this.zipCode = zipCode;
+	}
+	
+
+}
