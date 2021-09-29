7ddae4a6971ea8b1aecdd08feddad9c79e6da7fd
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-29 21:12:42 +0530

    ADDED: contructor for contact

--- src/main/com/bridgelabz/addressbooksystem/Contact.java
+++ src/main/com/bridgelabz/addressbooksystem/Contact.java
@@ -8,20 +8,13 @@ public class Contact {
 	private String firstName;
 	@CsvBindByName
 	private String lastName;
-	@CsvBindByName
-	private String address;
-	@CsvBindByName
-	private String city;
-	@CsvBindByName
-	private String state;
-	@CsvBindByName
-	private int zipCode;
+	Address contactAddress;
 	@CsvBindByName
 	private long phoneNumber;
 	@CsvBindByName
 	private String emailId;
 	
-	Address contactAddress;
+	
 	
 	public Contact(String firstName,String lastName,String address,String city,String state,int zipCode,long phoneNumber,String emailId){
 		this.firstName=firstName;
@@ -30,7 +23,13 @@ public class Contact {
 		this.phoneNumber=phoneNumber;
 		this.emailId=emailId;
 	}
-	
+	public Contact(String firstName,String lastName,Address address, long phoneNumber,String emailId) {
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.contactAddress=address;
+		this.phoneNumber=phoneNumber;
+		this.emailId=emailId;
+	}
 	
 	public String getFirstName() {
 		return firstName;
@@ -43,30 +42,6 @@ public class Contact {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public int getZipCode() {
-		return zipCode;
-	}
-	public void setZipCode(int zipCode) {
-		this.zipCode = zipCode;
 	}
 	public long getPhoneNumber() {
 		return phoneNumber;
@@ -86,10 +61,7 @@ public class Contact {
         return "Contact{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", address='" + address + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip=" + zipCode +
+                ", address='" + contactAddress + '\'' +
                 ", phoneNumber=" + phoneNumber +
                 ", email='" + emailId + '\'' +
                 '}';
