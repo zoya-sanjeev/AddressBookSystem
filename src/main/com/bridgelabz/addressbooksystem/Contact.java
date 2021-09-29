381e42f7719aa6d50bf2cc64dec25688cb44ffaa
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-29 21:16:53 +0530

    MODIFIED: contructors for contact class

--- src/main/com/bridgelabz/addressbooksystem/Contact.java
+++ src/main/com/bridgelabz/addressbooksystem/Contact.java
@@ -8,28 +8,40 @@ public class Contact {
 	private String firstName;
 	@CsvBindByName
 	private String lastName;
-	Address contactAddress;
+	@CsvBindByName
+	private String address;
+	@CsvBindByName
+	private String city;
+	@CsvBindByName
+	private String state;
+	@CsvBindByName
+	private int zipCode;
 	@CsvBindByName
 	private long phoneNumber;
 	@CsvBindByName
 	private String emailId;
 	
-	
+	Address contactAddress;
 	
 	public Contact(String firstName,String lastName,String address,String city,String state,int zipCode,long phoneNumber,String emailId){
 		this.firstName=firstName;
 		this.lastName=lastName;
-		this.contactAddress=new Address(address,city,state,zipCode);
+		this.address=address;
+		this.city=city;
+		this.state=state;
+		this.zipCode=zipCode;
 		this.phoneNumber=phoneNumber;
 		this.emailId=emailId;
 	}
-	public Contact(String firstName,String lastName,Address address, long phoneNumber,String emailId) {
+	
+	public Contact(String firstName,String lastName,Address address,long phoneNumber,String emailId) {
 		this.firstName=firstName;
 		this.lastName=lastName;
 		this.contactAddress=address;
 		this.phoneNumber=phoneNumber;
 		this.emailId=emailId;
 	}
+	
 	
 	public String getFirstName() {
 		return firstName;
@@ -42,6 +54,30 @@ public class Contact {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
 	}
 	public long getPhoneNumber() {
 		return phoneNumber;
@@ -61,7 +97,10 @@ public class Contact {
         return "Contact{" +
                 "firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", address='" + contactAddress + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zipCode +
                 ", phoneNumber=" + phoneNumber +
                 ", email='" + emailId + '\'' +
                 '}';
