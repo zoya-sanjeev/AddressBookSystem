3a5761c05ab1096c4aadb51f422254d5d8f77a20
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-29 21:08:17 +0530

    MODIFIED: contact contructor changed according to address class

--- src/main/com/bridgelabz/addressbooksystem/Contact.java
+++ src/main/com/bridgelabz/addressbooksystem/Contact.java
@@ -21,18 +21,16 @@ public class Contact {
 	@CsvBindByName
 	private String emailId;
 	
-	Address contactAddressS;
+	Address contactAddress;
 	
 	public Contact(String firstName,String lastName,String address,String city,String state,int zipCode,long phoneNumber,String emailId){
 		this.firstName=firstName;
 		this.lastName=lastName;
-		this.address=address;
-		this.city=city;
-		this.state=state;
-		this.zipCode=zipCode;
+		this.contactAddress=new Address(address,city,state,zipCode);
 		this.phoneNumber=phoneNumber;
 		this.emailId=emailId;
 	}
+	
 	
 	public String getFirstName() {
 		return firstName;
