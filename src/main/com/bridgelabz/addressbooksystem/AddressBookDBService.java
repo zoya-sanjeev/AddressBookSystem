57de401a6821a8a88fdd8111e4806c3b43530b5a
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-30 16:00:44 +0530

    ADDED: fucntions to getConnection, readData and getData

--- src/main/com/bridgelabz/addressbooksystem/AddressBookDBService.java
+++ src/main/com/bridgelabz/addressbooksystem/AddressBookDBService.java
@@ -1,5 +1,64 @@
 package main.com.bridgelabz.addressbooksystem;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.DriverManager;
+
 public class AddressBookDBService {
+	
+	private PreparedStatement contactStatement;
+	
+	private Connection getConnection() throws SQLException {
+		String jdbcURL="jdbc:mysql://localhost:3306/addressBookService?useSSL=false";
+		String userName="root";
+		String password="abcd1234";
+		Connection connection;
+		System.out.println("Connecting to database"+ jdbcURL);
+		connection=DriverManager.getConnection(jdbcURL,userName,password);
+		System.out.println("Connection is successfull"+ connection);
+		return connection;
+	}
+	
+	private List<Contact> getcontactData(ResultSet resultSet) {
+		List<Contact> contactList = new ArrayList<>();
+		try {
+			while(resultSet.next()) {
+				String firstName=resultSet.getString("first_name");
+				String lastName=resultSet.getString("last_name");
+				long phoneNumber=resultSet.getLong("phone_number");
+				String email=resultSet.getString("email");
+				String address=resultSet.getString("address");
+				String city=resultSet.getString("city");
+				String state=resultSet.getString("state");
+				int zipCode=resultSet.getInt("zipCode");
+				contactList.add(new Contact(firstName,lastName,new Address(address,city,state,zipCode),phoneNumber,email)); 		
+			}
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
+	
+	public List<Contact> readData(String name) throws SQLException{
+		String sql = String.format("select c.first_name, c.last_name, c.phone_number, c.email, a.address, a.city,a.state, a.zipCode"
+				+ " from contact c, address	 a, addressbook ab, contact_type t"
+				+ " where c.contact_id=t.contact_id and ab.addressbook_id=t.addressbook_id and c.contact_id=a.contact_id"
+				+ " and ab.addressbook_name='%s';",name);
+		List<Contact> contactList= new ArrayList<>();
+		try(Connection connection =this.getConnection()) {
+			Statement statement = connection.createStatement();
+			ResultSet result = statement.executeQuery(sql);
+			contactList= this.getcontactData(result);
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return contactList;
+	}
 
 }
